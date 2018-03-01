e263ae34dc21a6d8bc029a81e10d848a61f5138a
Hugo Knorr <hugo.knorr@gmail.com>
2018-03-01 23:02:48 +0100

    Add login form base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,33 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { AuthentificationService } from './authentification.service';
 import { TokenInterceptor } from './token.interceptor';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-  ],
-  providers: [
-    HttpClient,
-    AuthentificationService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: TokenInterceptor,
-      multi: true
-    }
-  ],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginComponent
+    ],
+    imports: [
+        BrowserModule,
+        HttpClientModule,
+        FormsModule,
+        ReactiveFormsModule
+    ],
+    providers: [
+        HttpClient,
+        AuthentificationService,
+        {
+            provide: HTTP_INTERCEPTORS,
+            useClass: TokenInterceptor,
+            multi: true
+        }
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule {
 }
--- src/app/token.interceptor.ts
+++ src/app/token.interceptor.ts
@@ -9,8 +9,9 @@ export class TokenInterceptor implements HttpInterceptor {
     }
 
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
+        const url = 'https://localhost:8443';
         request = request.clone({
+            url: url + request.url,
             setHeaders: {
                 Authorization: `Bearer ${this.auth.getAccessToken()}`
             }
