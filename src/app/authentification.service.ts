b1e323aae896284cfaf1f3feddaf4c33a548f455
Hugo Knorr <hugo.knorr@gmail.com>
2018-03-10 00:46:33 +0100

    Integration of login page and base structure

--- src/app/authentification.service.ts
+++ src/app/authentification.service.ts
@@ -1,15 +1,23 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
+let jwtDecode = require('jwt-decode');
 
 @Injectable()
 export class AuthentificationService {
+    onUserSet = new EventEmitter();
 
     constructor(private http: HttpClient) {
     }
 
-    login (username: string, password: string): Observable<Object> {
+    login(username: string, password: string): Observable<Object> {
         return this.http.post('/login_check', {username, password});
+    }
+
+    logout() {
+        sessionStorage.clear();
+        localStorage.clear();
+        this.onUserSet.emit(this.getCurrentUser());
     }
 
     public getAccessToken() {
@@ -20,10 +28,16 @@ export class AuthentificationService {
     }
 
     public setAccessToken(token, remember) {
-      if (remember) {
-        localStorage.setItem('access_token', token);
-      } else {
-        sessionStorage.setItem('access_token', token);
-      }
+        if (remember) {
+            localStorage.setItem('access_token', token);
+        } else {
+            sessionStorage.setItem('access_token', token);
+        }
+
+        this.onUserSet.emit(this.getCurrentUser());
+    }
+
+    public getCurrentUser() {
+        return this.getAccessToken() ? jwtDecode(this.getAccessToken()) : null;
     }
 }
