43b458555e495c5d44b749faa2d29154eeb61b0e
Unknown <ozturk.perso@gmail.com>
2018-04-19 16:33:11 +0200

    Add duplicate quotation

--- src/backend/model/Quotation.ts
+++ src/backend/model/Quotation.ts
@@ -2,26 +2,4 @@ import { QuotationBase } from './base/QuotationBase';
 
 export class Quotation extends QuotationBase {
     id: number;
-
-    getDuplicateQuotation(duplicateQuotation, quotation) {
-        duplicateQuotation.label = quotation.label;
-        duplicateQuotation.codeQuotation = quotation.codeQuotation;
-        duplicateQuotation.enabledCgv = quotation.enabledCgv;
-        duplicateQuotation.customerNotes = quotation.customerNotes;
-        duplicateQuotation.privateNotes = quotation.privateNotes;
-        duplicateQuotation.setContact(quotation.contact.id);
-        duplicateQuotation.setCustomer(quotation.customer.id);
-
-        if (quotation.paymentConditions) {
-            duplicateQuotation.setPaymentConditions(quotation.paymentConditions.id);
-        }
-        if (quotation.provision) {
-            duplicateQuotation.setProvision(quotation.provision.id);
-        }
-        if (quotation.status) {
-            duplicateQuotation.setStatus(quotation.status.id);
-        }
-
-        return duplicateQuotation;
-    }
 }
