14ad16c6b9519aaef06c409f0866eb5eb81f89a0
Dylan DUFAY <d.dufay@mentalworks.fr>
2018-06-13 10:08:27 +0200

    Export pdf

--- src/app/quotation/quotation-show/quotation-show.component.ts
+++ src/app/quotation/quotation-show/quotation-show.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 import { Quotation } from '../../../backend/model';
 import { QuotationsService } from '../../../backend/services';
 import { ActivatedRoute } from '@angular/router';
+import * as jsPDF from 'jspdf';
 
 @Component({
   selector: 'app-quotation-show',
@@ -9,6 +10,7 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./quotation-show.component.scss']
 })
 export class QuotationShowComponent implements OnInit {
+    @ViewChild('pdf') pdf: ElementRef;
     quotation: Quotation;
 
     constructor(
@@ -21,5 +23,12 @@ export class QuotationShowComponent implements OnInit {
         this.activatedRoute.params.subscribe(params => {
                 this.quotationsService.get(params.id).subscribe(quotation => this.quotation = quotation);
         });
+    }
+
+    exportPdf() {
+        const pdf = new jsPDF();
+        const content = this.pdf.nativeElement;
+        pdf.fromHTML(content.innerHTML, 15, 15, {});
+        pdf.save('test.pdf');
     }
 }
